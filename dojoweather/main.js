7b0cafb31cc9c659e9ab8b8b7efe861b69873d1e
Stephanie Puertas <80504918+stephaniepuertas@users.noreply.github.com>
2022-09-21 14:57:16 -0700

    Add files via upload

---
+++ dojoweather/main.js
@@ -0,0 +1,21 @@
+function toggle(element){
+    if(element.value === "Fah"){
+        var temp = document.querySelector(".two")
+        var temp2 = document.querySelector(".three")
+        var temp3 = document.querySelector(".four")
+        var temp4 = document.querySelector(".five")
+        var temp5 = document.querySelector(".six")
+        var temp6 = document.querySelector(".seven")
+        var temp7 = document.querySelector(".eight")
+        var temp8 = document.querySelector(".nine")
+        temp.innerText = "70º"
+        temp2.innerText = "75º"
+        temp3.innerText = "70º"
+        temp4.innerText = "75º"
+        temp5.innerText = "70º"
+        temp6.innerText = "75º"
+        temp7.innerText = "70º"
+        temp8.innerText = "75º"
+        temp9.innerText = "70º"
+    }
+}
